0710400195503d5c9323e1a7492e41cca2afa1d9
erikdevos <mrcollaborator@gmail.com>
2023-03-27 21:49:12 +0200

    Add Github link to footer.

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <section class="page-row-wrapper main-footer-section">
 	<footer class="content-wrapper column">
 		<div class="footer-content">
-		<?php echo date("Y"); ?> | Erik de Vos | <a href="https://www.linkedin.com/in/erikdevos/" target="_blank">LinkedIn</a>
+		<?php echo date("Y"); ?> | Erik de Vos | <a href="https://www.linkedin.com/in/erikdevos/" target="_blank">LinkedIn</a> | <a href="https://github.com/erikdevos/portfolio" target="_blank">Github</a>
 		</div>
 		
 	</footer>
